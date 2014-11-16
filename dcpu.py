8228650ce977a39268031e141c27773dfd07696c
Andrew Gorcester <andrew.gorcester@gmail.com>
2014-11-16 00:01:20 -0800

    initial commit

---
+++ dcpu.py
@@ -0,0 +1,204 @@
+from enum import Enum
+
+class Opcode(Enum):
+    NONBASIC = 0x0
+    SET = 0x1
+    ADD = 0x2
+    SUB = 0x3
+    MUL = 0x4
+    DIV = 0x5
+    MOD = 0x6
+    SHL = 0x7
+    SHR = 0x8
+    AND = 0x9
+    BOR = 0xa
+    XOR = 0xb
+    IFE = 0xc
+    IFN = 0xd
+    IFG = 0xe
+    IFB = 0xf
+
+class NonBasicOpcode(Enum):
+    JSR = 0x01
+
+OPCODE_COST = {
+    Opcode.SET: 1,
+    Opcode.ADD: 2,
+    Opcode.SUB: 2,
+    Opcode.MUL: 2,
+    Opcode.DIV: 3,
+    Opcode.MOD: 3,
+    Opcode.SHL: 2,
+    Opcode.SHR: 2,
+    Opcode.AND: 1,
+    Opcode.BOR: 1,
+    Opcode.XOR: 1,
+    Opcode.IFE: 2, # plus 1 if test fails
+    Opcode.IFN: 2, # plus 1 if test fails
+    Opcode.IFG: 2, # plus 1 if test fails
+    Opcode.IFB: 2, # plus 1 if test fails
+    NonBasicOpcode.JSR: 2,
+}
+
+def sanitized_value(value, word_length):
+    if not isinstance(value, int):
+        value = int(value)
+    value = value % 2**word_length
+    return value
+
+def does_overflow(value, word_length):
+    return not 0 <= value < 2**word_length
+
+class RAM():
+    # initial_contents must be a list of words!
+    def __init__(self, word_length, size, initial_contents=None):
+        self.word_length = word_length
+        self.contents = [0x0000 for _ in range(size)]
+        if initial_contents:
+            assert len(initial_contents) <= len(self.contents)
+            self.contents[:len(initial_contents)] = initial_contents
+
+    @property
+    def size(self):
+        return len(self.contents)
+
+    def get(self, pos):
+        return self.contents[pos]
+
+    def set(self, pos, value):
+        value = sanitized_value(value, self.word_length)
+        self.contents[pos] = value
+
+class DCPURegisterBank():
+
+    all_regs = ('a', 'b', 'c', 'x', 'y', 'z', 'i', 'j', 'pc', 'sp', 'o')
+    regs = all_regs[:8]
+
+    def __init__(self, word_length, values):
+        self.word_length = word_length
+        for reg in self.all_regs:
+            self[reg] = values[reg]
+
+    def __len__(self):
+        return len(self.all_regs)
+
+    def __getitem__(self, key):
+        if key in self.all_regs:
+            return getattr(self, key)
+        else:
+            raise KeyError
+
+    def __setitem__(self, key, value):
+        if key in self.all_regs:
+            value = sanitized_value(value, self.word_length)
+            setattr(self, key, value)
+        else:
+            raise KeyError
+
+    def __iter__(self):
+        return iter(self.all_regs)
+
+    def __contains__(self, key):
+        return key in self.all_regs
+
+    def items(self):
+        for key in self.all_regs:
+            return (key, getattr(self, key))
+
+class CPU():
+    # initial_registers must be a dictionary with a, b, c, x, y, z, i, j, pc, sp, o.
+    def __init__(self, initial_registers=None, initial_ram=None, initial_cycle=0):
+        if not initial_ram:
+            initial_ram = RAM(word_length=16, size=2**16)
+
+        if not initial_registers:
+            initial_registers = {key: 0x0000 for key in DCPURegisterBank.all_regs}
+
+        self.reg = DCPURegisterBank(word_length=16, values=initial_registers)
+        self.ram = initial_ram
+        self.cycle = initial_cycle
+
+        self.value_codes = {}
+        self.value_codes.update({x: lambda: self.reg[self.reg.regs[x]] for x in range(0x00, 0x08)})
+        self.value_codes.update({x + 0x08: lambda: self.ram.get(self.reg[self.reg.regs[x]]) for x in range(0x00, 0x08)})
+        self.value_codes.update({x + 0x10: lambda: self.ram.get(self.add(self.next_word(), self.reg[self.reg.regs[x]])) for x in range(0x00, 0x08)})
+
+        self.value_codes.update({
+            0x18: lambda: self.pop(),
+            0x19: lambda: self.ram.get(self.reg.sp),
+            0x1a: lambda: self.push(),
+            0x1b: lambda: self.reg.sp,
+            0x1c: lambda: self.reg.pc,
+            0x1d: lambda: self.reg.o,
+            0x1e: lambda: self.ram.get(self.next_word()),
+            0x1f: lambda: self.next_word(),
+        })
+
+        for x in range(0x20):
+            self.value_codes[x + 0x20] = lambda: x
+
+        self.set_codes = {}
+        self.set_codes.update({x: lambda value: self.reg.__setitem__(self.reg.regs[x], value) for x in range(0x00, 0x08)})
+        self.set_codes.update({x + 0x08: lambda value: self.ram.set(self.reg[self.reg.regs[x]], value) for x in range(0x00, 0x08)})
+        self.set_codes.update({x + 0x10: lambda value: self.ram.set(self.add(self.next_word(), self.reg[self.reg.regs[x]]), value) for x in range(0x00, 0x08)})
+
+        self.set_codes.update({
+            0x18: lambda value: self.pop(value),
+            0x19: lambda value: self.ram.set(self.reg.sp, value),
+            0x1a: lambda value: self.push(value),
+            0x1b: lambda value: self.regs.__setitem__('sp', value),
+            0x1c: lambda value: self.regs.__setitem__('pc', value),
+            0x1d: lambda value: self.regs.__setitem__('o', value),
+            0x1e: lambda value: self.ram.set(self.next_word(), value),
+            0x1f: lambda value: None,
+        })
+
+        for x in range(0x20):
+            self.set_codes[x + 0x20] = lambda value: None
+
+    def step(self):
+        # read PC
+        word = self.next_word()
+        b, a, o = word[0:5], word[6:11], word[12:15]
+        opcode = Opcode(int(o))
+        if opcode == Opcode.NONBASIC:
+            opcode = NonBasicOpcode(int(a))
+
+        # execute instruction
+        self.execute(opcode, a, b)
+
+    def next_word(self):
+        word = self.ram.get(self.pc)
+        self.pc += 1
+        return word
+
+    def pop(self, value=None):
+        if value:
+            self.ram.set(self.sp, value)
+        else:
+            value = self.ram.get(self.sp)
+        sp += 1
+        return value
+
+    def push(self, value=None):
+        sp -= 1
+        if value:
+            self.ram.set(self.sp, value)
+        else:
+            value = self.ram.get(self.sp)
+        return value
+
+    def get_by_code(self, code):
+        if (0x10 <= code <= 0x17) or code in [0x1e, 0x1f]:
+            self.cycle += 1
+        return self.value_codes[code]()
+
+    def set_by_code(self, code, value):
+        if (0x10 <= code <= 0x17) or code in [0x1e, 0x1f]:
+            self.cycle += 1
+        self.set_codes[code](value)
+
+    def execute(self, opcode, a, b):
+        cycle += OPCODE_COST[opcode]
+        # run thingie
+        # profit
---
+++ test_dcpu.py
@@ -0,0 +1,161 @@
+import dcpu
+from bitarray import bitarray
+import pytest
+
+@pytest.fixture
+def cpu():
+    return dcpu.CPU()
+
+@pytest.fixture
+def ram():
+    return dcpu.RAM(word_length=16, size=0x10000)
+
+def test_ram_init():
+    assert len(dcpu.RAM(word_length=8, size=0x1000).contents) == 0x1000
+    assert len(dcpu.RAM(word_length=16, size=0x20000).contents) == 0x20000
+    ram = dcpu.RAM(word_length=16, size=0x1000, initial_contents=[0, 1, 2, 3])
+    assert ram.get(0) == 0
+    assert ram.get(1) == 1
+    assert ram.get(2) == 2
+    assert ram.get(3) == 3
+    assert ram.get(4) == 0
+
+def test_ram_set(ram):
+    ram.set(0x01, 0xffff) # maximum word length
+    assert ram.get(0x01) == 0xffff
+    with pytest.raises(TypeError):
+        ram.set(0x01, pytest)
+    ram.set(0x01, 2**ram.word_length + 0x10) # more than maximum word length
+    assert ram.get(0x01) == (2**ram.word_length + 0x10) % 2**ram.word_length
+    ram.set(0x01, -0x10)
+    assert ram.get(0x01) == -0x10 % 2**ram.word_length
+    with pytest.raises(IndexError):
+        ram.set(0xffffffff, 0x0000)
+
+def test_cpu_init():
+    cpu = dcpu.CPU()
+    for reg in cpu.reg:
+        assert cpu.reg[reg] == 0
+    assert cpu.ram.word_length == 16
+    assert cpu.ram.size == 0x10000
+    assert cpu.ram.get(0) == 0
+
+    initial_ram = dcpu.RAM(word_length=16, size=0x10000, initial_contents=[0x0000, 0x0000, 0xffff, 0xffff, 0x0001])
+    initial_registers = {'a': 0x1000,
+                         'b': 0x2000,
+                         'c': 0x3000,
+                         'x': 0x4000,
+                         'y': 0x5000,
+                         'z': 0x6000,
+                         'i': 0x7000,
+                         'j': 0x8000,
+                         'pc': 0x9000,
+                         'sp': 0xa000,
+                         'o': 0x1,}
+    initial_cycle = 2
+    cpu = dcpu.CPU(initial_registers=initial_registers, initial_ram=initial_ram, initial_cycle=initial_cycle)
+
+    for reg in cpu.reg:
+        assert cpu.reg[reg] == initial_registers[reg]
+    assert cpu.cycle == initial_cycle
+
+    assert cpu.ram.get(0) == 0x0000
+    assert cpu.ram.get(1) == 0x0000
+    assert cpu.ram.get(2) == 0xffff
+    assert cpu.ram.get(3) == 0xffff
+    assert cpu.ram.get(4) == 0x0001
+    assert cpu.ram.get(5) == 0x0000
+
+def test_register_bank():
+    initial_registers = {'a': 0x1000,
+                         'b': 0x2000,
+                         'c': 0x3000,
+                         'x': 0x4000,
+                         'y': 0x5000,
+                         'z': 0x6000,
+                         'i': 0x7000,
+                         'j': 0x8000,
+                         'pc': 0x9000,
+                         'sp': 0xa000,
+                         'o': 0x1,}
+    word_length = 16
+    regbank = dcpu.DCPURegisterBank(word_length=word_length, values=initial_registers)
+
+    test_value = 0x1010
+
+    for reg in initial_registers:
+        assert regbank[reg] == initial_registers[reg]
+        assert getattr(regbank, reg) == initial_registers[reg]
+        assert regbank.__getitem__(reg) == initial_registers[reg]
+        regbank[reg] = test_value
+        assert regbank[reg] == test_value
+        regbank[reg] = 2**word_length + 0x10 # more than maximum word length
+        assert regbank[reg] == (2**word_length + 0x10) % 2**word_length
+        regbank[reg] = -1
+        assert regbank[reg] == -1 % 2**word_length
+
+    with pytest.raises(KeyError):
+        regbank['nonsense'] = test_value
+
+    with pytest.raises(KeyError):
+        assert regbank['nonsense'] == 0x0000
+
+# "additional" is a function, to which the CPU will be passed as an argument and which must evaluate to true on success. It is optional.
+#@pytest.mark.parametrize(("set_code_value_pairs", "get_code", "expected", "additional"), [
+#    ([(0x00, 0xffff)], 0x00, 0xffff, lambda cpu: cpu.reg['a'] == 0xffff),
+    # ([(0x01, 0xffff)], 0x01, 0xffff, lambda cpu: cpu.reg['b'] == 0xffff),
+    # ([(0x02, 0xffff)], 0x02, 0xffff, lambda cpu: cpu.reg['c'] == 0xffff),
+    # ([(0x03, 0xffff)], 0x03, 0xffff, lambda cpu: cpu.reg['x'] == 0xffff),
+    # ([(0x04, 0xffff)], 0x04, 0xffff, lambda cpu: cpu.reg['y'] == 0xffff),
+    # ([(0x05, 0xffff)], 0x05, 0xffff, lambda cpu: cpu.reg['z'] == 0xffff),
+    # ([(0x06, 0xffff)], 0x06, 0xffff, lambda cpu: cpu.reg['i'] == 0xffff),
+    # ([(0x07, 0xffff)], 0x07, 0xffff, lambda cpu: cpu.reg['j'] == 0xffff),
+    # ([(0x00, 0x0002), (0x08, 0xffff)], 0x08, 0xffff, lambda cpu: cpu.ram.get(0x0002) == 0xffff),
+    # ([(0x01, 0x0002), (0x09, 0xffff)], 0x09, 0xffff, lambda cpu: cpu.ram.get(0x0002) == 0xffff),
+    # ([(0x02, 0x0002), (0x0a, 0xffff)], 0x0a, 0xffff, lambda cpu: cpu.ram.get(0x0002) == 0xffff),
+    # ([(0x03, 0x0002), (0x0b, 0xffff)], 0x0b, 0xffff, lambda cpu: cpu.ram.get(0x0002) == 0xffff),
+    # ([(0x04, 0x0002), (0x0c, 0xffff)], 0x0c, 0xffff, lambda cpu: cpu.ram.get(0x0002) == 0xffff),
+    # ([(0x05, 0x0002), (0x0d, 0xffff)], 0x0d, 0xffff, lambda cpu: cpu.ram.get(0x0002) == 0xffff),
+    # ([(0x06, 0x0002), (0x0e, 0xffff)], 0x0e, 0xffff, lambda cpu: cpu.ram.get(0x0002) == 0xffff),
+    # ([(0x07, 0x0002), (0x0f, 0xffff)], 0x0f, 0xffff, lambda cpu: cpu.ram.get(0x0002) == 0xffff),
+#    ([(0x1c, 0x0010), (0x00, 0x0010), (0x08], 0x0f, 0xffff, lambda cpu: cpu.ram.get(0x0002) == 0xffff),
+#    ])
+# def test_cpu_opcodes(cpu, set_code_value_pairs, get_code, expected, additional):
+#     for pair in set_code_value_pairs:
+#         cpu.set_by_code(*pair)
+#     assert cpu.get_by_code(get_code) == expected
+#     if additional:
+#         assert additional(cpu)
+
+@pytest.mark.parametrize(('set_code', 'get_code', 'reg'), [
+    (0x00, 0x00, 'a'),
+    # (0x01, 0x01, 'b'),
+    # (0x02, 0x02, 'c'),
+    # (0x03, 0x03, 'x'),
+    # (0x04, 0x04, 'y'),
+    # (0x05, 0x05, 'z'),
+    # (0x06, 0x06, 'i'),
+    # (0x07, 0x07, 'j'),
+    ])
+def test_cpu_register_opcodes(cpu, set_code, get_code, reg):
+    value = 0x0101
+    cpu.set_by_code(set_code, value)
+    assert cpu.get_by_code(get_code) == value
+    assert cpu.reg[reg] == value
+
+# @pytest.mark.parametrize(('set_code', 'get_code'), [
+#     (0x08, 0x08, 'a'),
+#     (0x09, 0x09, 'b'),
+#     (0x0a, 0x0a, 'c'),
+#     (0x0b, 0x0b, 'x'),
+#     (0x0c, 0x0c, 'y'),
+#     (0x0d, 0x0d, 'z'),
+#     (0x0e, 0x0e, 'i'),
+#     (0x0f, 0x0f, 'j'),
+#     ])
+# def test_cpu_register_memory_opcodes(cpu, set_code, get_code, reg):
+#     memloc = 0x0010
+#     value = 0xfafa
+#     cpu.mem.set(memloc, value)
+#     cpu.reg[reg] = memloc
+    
